79a1b8c901cf015b8e3934edf448ad2e2aad5d04
Skrewild <143341219+Skrewild@users.noreply.github.com>
2025-06-28 01:44:57 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,10 +3,4 @@ const api = axios.create({
   baseURL: "https://shop-kw6q.onrender.com",
 });
 
-export function adminApi(secret) {
-  return axios.create({
-    baseURL: "https://shop-kw6q.onrender.com",
-    headers: { "x-admin-secret": secret }
-  });
-}
 export default api;
