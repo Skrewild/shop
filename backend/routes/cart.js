f7091e183d11bed1491d362f58324aaa011e0e62
Skrewild <143341219+Skrewild@users.noreply.github.com>
2025-06-29 19:30:28 +0500

    Update cart.js

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getCart, addToCart, removeFromCart } = require('../controllers/cartController');
+const { getCart, addToCart, removeFromCart, confirmCart } = require('../controllers/cartController');
 
 router.get('/', getCart);
 router.post('/', addToCart);
 router.delete('/:id', removeFromCart);
+router.post('/confirm', confirmCart);
 
 module.exports = router;
